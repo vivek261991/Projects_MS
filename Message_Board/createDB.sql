870b9c30835b898515b7e0c6e31db9468b5c39d5
vivek261991 <vivek.vaidyanathasubramani@mavs.uta.edu>
2016-05-23 22:17:40 -0500

    Adding Db file

---
+++ Message_Board/createDB.sql
@@ -0,0 +1,14 @@
+create table users (
+   username   varchar(10) primary key,
+   password   varchar(32),
+   fullname   varchar(45),
+   email      varchar(45)
+);
+
+create table posts (
+   id         varchar(13) primary key,
+   postedby   varchar(10),
+   follows    varchar(13),
+   datetime   datetime,
+   message    text
+);
